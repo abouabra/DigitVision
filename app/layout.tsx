196493dc6448347a18bb937271a9c5dfdc34fe01
abouabra <aymanbouabra@gmail.com>
2025-03-21 18:32:09 +0000

    moved the Footer to Home

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 					enableSystem={false}
 					forcedTheme="dark">
 
-          {/* Background */}
+          			{/* Background */}
 					<div className="fixed inset-0 overflow-hidden pointer-events-none">
 						<div className="absolute top-0 left-0 w-1/3 h-1/3 bg-purple-500/15 rounded-full blur-3xl transform -translate-x-1/2 -translate-y-1/2"></div>
 						<div className="absolute bottom-0 right-0 w-1/2 h-1/2 bg-blue-500/15 rounded-full blur-3xl transform translate-x-1/3 translate-y-1/3"></div>
